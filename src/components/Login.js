dafb9fda2d1a2d5136d0c411d4b197193928a7f3
Nayara Covas <nayaracovas1@outlook.com>
2023-06-24 19:05:19 +0200

    studing react

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import './login.css';
+
+export default function Login({title}) {
+   return (
+        <div className="login">
+            <div className="test">
+
+
+            </div>
+            <h1>{title}</h1>
+            <a href="/login">Login</a>
+            <a href="/signup">Sign up</a>
+        </div>
+   )
+}
+
